26dcb55cfaa600bf97154b7961f2e4e608ca623d
moriTakito <mori_takito@koushi-intec.co.jp>
2025-04-08 13:04:45 +0900

    課題15の解答アップロード

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,24 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	
+	private int gear = 1;
+	private int speed = 10;
+	
+	public void geerChange(int afterGear) {
+		int oldGear = gear;
+		if (afterGear > 0 && afterGear <= 5) {
+			gear = afterGear;
+		}else {
+			System.out.println("異常な入力値を検出したためギア1に切り替えます");
+			gear = 1;
+		}
+		speed = gear * 10;
+		System.out.println("ギア" + oldGear + "から" + gear + "に切り替えました");
+	}
+	
+	public void run() {
+		System.out.println("速度は時速" + speed + "kmです");
+	}
+
+}
